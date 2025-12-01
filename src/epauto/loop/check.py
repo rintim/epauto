4a43f6cb19a6fc4711bc9d792defcaa742297ec0
rintim <rintim@foxmail.com>
2025-12-02 00:42:00 +0800

    pref: handle timeout in sperate except block.

--- src/epauto/loop/check.py
+++ src/epauto/loop/check.py
@@ -18,15 +18,16 @@ async def execute(cfg: Config) -> LoopState:
         async with ClientSession(
             headers=header, timeout=ClientTimeout(total=cfg.connect.test_timeout)
         ) as session:
-            try:
-                async with session.get(cfg.connect.test_url) as response:
-                    if response.status == 200:
-                        logger.info("Already connected to the internet.")
-                        return LoopState.CONNECT
-                    else:
-                        raise Exception(f"Unexpected status code: {response.status}")
-            except TimeoutError:
-                raise Exception(f"Connection to {cfg.connect.test_url} timed out.")
+            async with session.get(cfg.connect.test_url) as response:
+                if response.status == 200:
+                    logger.info("Already connected to the internet.")
+                    return LoopState.CONNECT
+                else:
+                    raise Exception(f"Unexpected status code: {response.status}")
+
+    except TimeoutError:
+        logger.info("Check failed: Timeout. Proceeding to login.")
+        return LoopState.LOGIN
 
     except Exception as e:
         logger.info('Check failed: [%s]"%s". Proceeding to login.', type(e).__name__, e)
